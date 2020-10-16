a59a990f9869ae1ac8b5ad4724b55c38bf2a24ee
Kathryn Osygus <kathrynosygus@Kathryns-MacBook-Pro.local>
2020-10-15 21:08:21 -0400

    I worked on the home and culprits pages.

---
+++ js/navbar.js
@@ -0,0 +1,11 @@
+// Get Reference To Element
+var ul = document.querySelector("nav > ul");
+
+// Define a behavior
+function toggleMenu() {
+  ul.classList.toggle("active");
+}
+
+// Wire it up!
+
+// Use a shortcut and use the onclick attribute in HTML
